e83ce9b12bd4c6817424091abff522ec849125d4
Karthick Chandiran <kc@Karthick-MacBook-Air.local>
2022-08-10 16:38:46 +0530

    initial commit

---
+++ ASPEmpty/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ASPEmpty
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                DeveloperExceptionPageOptions dx = new DeveloperExceptionPageOptions { SourceCodeLineCount = 5 };
+                
+                app.UseDeveloperExceptionPage(dx);
+            }
+            
+            app.UseStaticFiles();
+            app.UseRouting();
+      
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    throw new Exception("Error occured...");
+                    await context.Response.WriteAsync("Hello world");
+                });
+            });
+            
+
+        }
+    }
+}
+
